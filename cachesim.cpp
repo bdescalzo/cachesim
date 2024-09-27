3035adb8d35fa6ba4ff1808603a4adf1a42a68f2
Beñat Descalzo <bdescalzo001@ikasle.ehu.eus>
2024-09-27 10:05:10 +0200

    Crear cachesim.cpp

---
+++ cachesim.cpp
@@ -0,0 +1,5 @@
+/*
+CACHESIM
+Autor: Beñat Descalzo Alcuaz
+Descripción: El siguiente código implementa un simulador sencillo de memoria caché, con soporte para diferentes protocolos de lectura/escritura y tamaños para los datos. Proyecto creado para la asignatura "Arquitectura de Computadores en la UPV/EHU.
+*/
